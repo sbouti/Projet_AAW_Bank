5a842eb884e27538691f773c35c48d5c575f343b
sbouti <50195807+sbouti@users.noreply.github.com>
2021-05-25 17:29:06 +0200

    Add files via upload

---
+++ bank/src/test/java/aaw/projet/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package aaw.projet.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
